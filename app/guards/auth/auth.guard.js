4135527ba1ff8b218708f9398de1b32344b1264b
Austin Willis <austin.willis@bullhorn.com>
2016-09-22 15:53:59 -0500

    help

---
+++ app/guards/auth/auth.guard.js
@@ -0,0 +1,24 @@
+import { CanActivate, Router } from "@angular/router";
+import { Injectable } from "@angular/core";
+import { AuthService } from '../../services/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(router: Router, auth: AuthService) {
+    this.router = router;
+    this.auth = auth;
+  }
+
+  canActivate() {
+    // return this.auth.auth$
+    //   .take(1)
+    //   .map(authState => !!authState)
+    //   .do(authenticated => {
+    //     if (!authenticated) {
+    //       this.router.navigate(['/']);
+    //     }
+    //   });
+    return false;
+  }
+}
